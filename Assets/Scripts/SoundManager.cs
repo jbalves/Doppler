e6a01e68ba911559f6e3db249000cdfd02802abd
Jeferson Barros <soldier.jhonson@gmail.com>
2017-02-21 09:54:06 -0400

    Initial project

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour {
+
+	[Header("References")]
+	public SoundScroll soundPrefab;
+
+	private GameObject cube;
+	private soundPrefab sound;
+
+	// Use this for initialization
+	void Start () {
+			cube = GameObject.FindGameObjectWithTag ("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		sound =  Instantiate(soundPrefab);
+		//soundObject.transform.position = player.transform.position;
+		
+	}
+}
